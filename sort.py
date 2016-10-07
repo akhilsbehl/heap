7a6bd4f1a037e59bb33c22a7331f29f2c79479f9
akhilsbehl <akhilsbehl@gmail.com>
2016-10-07 14:30:51 +0530

    Added test cases. Linting.

--- sort.py
+++ sort.py
@@ -1,13 +1,23 @@
 '''Implements heapsort.'''
 
 
+import random
 from heap import Heap
 
 
 def heapsort(array, increasing=True):
+    '''Implements sorting using heaps.'''
     order = {True: 'min', False: 'max'}[increasing]
     heap = Heap(array, order)
     sorted_array = []
     while heap.data:
         sorted_array.append(heap.pop())
     return sorted_array
+
+
+if __name__ == '__main__':
+    SIZE = int(random.uniform(0, 100))
+    SAMPLE = list((int(random.uniform(1, 100)) for x in range(SIZE)))
+    assert heapsort(SAMPLE) == sorted(SAMPLE)
+    assert heapsort(SAMPLE, increasing=False) == sorted(SAMPLE, reverse=True)
+    print('All tests passed! :)')
